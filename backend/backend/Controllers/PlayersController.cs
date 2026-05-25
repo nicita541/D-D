a32437c417056620b9bf31c15208b15f61e402bf
nicita541 <alexandervestrin@yandex.ru>
2026-05-25 17:52:22 +0700

    1

---
+++ backend/backend/Controllers/PlayersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet("/id")]
+        public ActionResult<Player> GetPlayers()
+        {
+           throw new NotImplementedException();
+        }
+
+    }
+}
